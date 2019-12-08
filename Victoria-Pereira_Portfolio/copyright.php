b06b95d557efe594fe4da83412f0c2051d9e0480
viperee <just.b.nd.l@gmail.com>
2019-12-08 13:40:36 +0100

    Migration footer en php

---
+++ Victoria-Pereira_Portfolio/copyright.php
@@ -0,0 +1,16 @@
+<footer class="footer-section p-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="copyright">© 2018 Barber Shop Powered by DynamicLayers</div>
+            </div>
+            <div class="col-4">
+                <ul class="nav">
+                    <li class="nav-item"><a class="nav-link" href="#">Orders</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Report Problem</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
--- Victoria-Pereira_Portfolio/index.php
+++ Victoria-Pereira_Portfolio/index.php
@@ -125,62 +125,14 @@
         <!--/SECTION Entre culture et tradition-->
     </div>
     <!--SECTION FOOTER-->
-    <section class="footer-section p-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-xs-12 xs-padding">
-                    <img class="navbar-brand" src="pictures/logo.png">
-                    <p class="footer-text">ADDRESS: 100 Tenth Avenue, New York City,1001
-                        <br>FOR BOOKING: (044) 359 0173</p>
-                    <ul class="nav nav-icons">
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-twitter"></i></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-instagram"></i></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 col-xs-12 xs-padding">
-                    <h4>Opening Hours</h4>
-                    <p class="footer-text">Monday - Friday 11:30AM - 2:00PM <br>Saturday - Sunday 4:30PM - 10:00PM <br>Monday
-                        - Friday 5:30PM - 11:00PM</p>
-                </div>
-                <div class="col-md-4 col-xs-12 xs-padding">
-                    <h4>Subscribe to our contents</h4>
-                    <div>
-                        <form action="#">
-                            <input type="email" name="email" class="form-input" placeholder="Email Address...">
-                            <button type="submit" class="submit">SUBSCRIBE</button>
-                        </form>
-                        <p class="align-left footer-text">There are design companies and then there are user experience
-                            design professional.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php include("footer.php");?>
     <!--/SECTION FOOTER-->
     <!--SECTION COPYRIGHT-->
-    <footer class="footer-section p-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-8">
-                    <div class="copyright">© 2018 Barber Shop Powered by DynamicLayers</div>
-                </div>
-                <div class="col-4">
-                    <ul class="nav">
-                        <li class="nav-item"><a class="nav-link" href="#">Orders</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Report Problem</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include("copyright.php"); ?>
     <!--/SECTION COPYRIGHT-->
 </div>
 <!--IMPORTS JS-->
-<?php include("script.php") ?>
+<?php include("script.php"); ?>
 <!--/IMPORTS JS-->
 </body>
 </html>
