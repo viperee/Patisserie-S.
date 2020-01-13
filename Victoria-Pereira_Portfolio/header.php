ba96797cf1a40e06f6ffe1ea4599d4a6cb9ef8c3
viperee <just.b.nd.l@gmail.com>
2020-01-13 20:41:08 +0100

    Page a propos slider todo + navbar in progress

--- Victoria-Pereira_Portfolio/header.php
+++ Victoria-Pereira_Portfolio/header.php
@@ -1,22 +1,22 @@
 <!--TODO Refaire la navbar-->
 <header>
-    <div class="container">
-        <nav class="navbar navbar-expand-md navbar-dark">
-            <img class="navbar-brand"
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-dark p-0">
+            <img class="navbar-brand offset-1"
                  src="pictures/MJM-UI_PEREIRA-VICTORIA_Logotype-Simple-Iconographique-Noir.png">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <div class="collapse navbar-collapse offset-5" id="navbarNav">
                 <ul class="nav navbar-nav">
-                    <li class="nav-item <?php echo ($page == 'accueil' ? "active" : "");?>">
+                    <li class="nav-item mr-5 <?php echo ($page == 'accueil' ? "active" : "");?>">
                         <a class="nav-link" href="index.php">Accueil</a>
                     </li>
-                    <li class="nav-item <?php echo ($page == 'patisseries' ? "active" : "");?>">
+                    <li class="nav-item mr-5 <?php echo ($page == 'patisseries' ? "active" : "");?>">
                         <a class="nav-link" href="patisseries.php">Les Pâtisseries</a>
                     </li>
-                    <li class="nav-item <?php echo ($page == 'apropos' ? "active" : "");?>">
+                    <li class="nav-item mr-5 <?php echo ($page == 'apropos' ? "active" : "");?>">
                         <a class="nav-link" href="apropos.php">A propos</a>
                     </li>
                     <li class="nav-item <?php echo ($page == 'contact' ? "active" : "");?>">
